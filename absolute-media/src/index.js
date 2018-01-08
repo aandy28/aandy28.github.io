e02f87b2d213be08898e7f1aa1461282b9530b45
Andrew Smith <andrew.smith@cef.co.uk>
2018-01-08 15:46:48 +0000

    old removed

--- absolute-media/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import Nav from './components/navigation/';
-import App from './components/home/App';
-ReactDOM.render(<span><Nav /><App /></span>, document.getElementById('root'));
